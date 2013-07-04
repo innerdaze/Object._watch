f97d4e2845779dc2635d3dff5630e1d9b19dfb27
lsdriscoll <lsdriscoll@gmail.com>
2013-07-04 21:29:25 +0100

    Added error catch in case of no Object.defineProperty support

--- Watchable.js
+++ Watchable.js
@@ -20,6 +20,10 @@
  * @constructor
  */
 
+try{ Object.defineProperty({}, 'x', {}) } catch (e){
+    console.warn("Error: You're using a weak browser that doesn't support Object.defineProperty. Write your own workaround.");
+}
+
 Watchable = function (props) {
 
     return Object.create(props||{}, {
